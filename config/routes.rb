6b1a03a65fb31e3162faf4618e6d6c83771e277c
Guillermo <gibsonguillermoarturo@gmail.com>
2017-08-09 21:25:29 -0400

    Agregando CanCanCan

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   resources :songs
   devise_for :users
+  
+  # resources :users do
+  #   member do
+  #     get 'songs#index'
+  #   end
+  # end
+  get 'mysongs' => 'songs#mysongs'
+
   get 'home/index'
 
-
-  root "home#index"
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
